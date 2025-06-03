cd3b128447eb8aa45e377e91f8c41f13cbf24206
KaiKimera <git@kai.kim>
2025-06-03 11:15:58 +0300

    Update resources

--- app_backup_wsb_mail.ps1
+++ app_backup_wsb_mail.ps1
@@ -3,8 +3,6 @@
 .GUID         18c25998-a474-425e-a59f-e32f79c8431d
 .AUTHOR       Kai Kimera
 .AUTHOREMAIL  mail@kai.kim
-.COMPANYNAME  Library Online
-.COPYRIGHT    2024 Library Online. All rights reserved.
 .TAGS         windows server backup mail
 .LICENSEURI   https://choosealicense.com/licenses/mit/
 .PROJECTURI   https://lib.onl/ru/2024/09/40539e36-4656-5532-b920-8975c97d4dc5/
@@ -19,10 +17,10 @@ The script sends messages to the specified address for further analysis.
 The messages contain the host ID and notification type.
 
 .EXAMPLE
-.\app.backup.wsb.mail.ps1 -Type 'error' [-SSL]
+.\app_backup_wsb_mail.ps1 -Type 'error' [-SSL]
 
 .EXAMPLE
-.\app.backup.wsb.mail.ps1 -Type 'success' [-SSL]
+.\app_backup_wsb_mail.ps1 -Type 'success' [-SSL]
 
 .LINK
 https://lib.onl/ru/2024/09/40539e36-4656-5532-b920-8975c97d4dc5/
