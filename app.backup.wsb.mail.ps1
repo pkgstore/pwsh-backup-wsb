39e947f167d6e172352b956760ef21e26e8280ab
KaiKimera <git@kai.kim>
2025-12-01 18:48:44 +0300

    Update resources

--- app.backup.wsb.mail.ps1
+++ app.backup.wsb.mail.ps1
@@ -2,11 +2,13 @@
 .VERSION      0.1.0
 .GUID         18c25998-a474-425e-a59f-e32f79c8431d
 .AUTHOR       Kai Kimera
-.AUTHOREMAIL  mail@kai.kim
+.AUTHOREMAIL  mail@kaikim.ru
 .TAGS         windows server backup mail
 .LICENSEURI   https://choosealicense.com/licenses/mit/
 .PROJECTURI   https://libsys.ru/ru/2024/09/40539e36-4656-5532-b920-8975c97d4dc5/
 #>
+
+#Requires -Version 7.2
 
 <#
 .SYNOPSIS
@@ -31,37 +33,55 @@ https://libsys.ru/ru/2024/09/40539e36-4656-5532-b920-8975c97d4dc5/
 # -------------------------------------------------------------------------------------------------------------------- #
 
 param(
-  [Parameter(HelpMessage='Message type.')]
+  [Alias('H', 'Host')]
+  [string]$Hostname = ([System.Net.Dns]::GetHostEntry($env:ComputerName).HostName),
+
   [ValidateSet('error', 'success')]
+  [Alias('T')]
   [string]$Type,
-  [Parameter(HelpMessage='Enable or disable encrypted connection.')]
-  [switch]$SSL = $false,
-  [Alias('Host')][string]$Hostname = ([System.Net.Dns]::GetHostByName([string]'localhost').HostName)
+
+  [Parameter(Mandatory)]
+  [Alias('S', 'Subj')]
+  [string]$Subject = "Windows Server Backup: ${Hostname}",
+
+  [Parameter(Mandatory)]
+  [Alias('F')]
+  [string]$From,
+
+  [Parameter(Mandatory)]
+  [ValidatePattern('^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{1,}$')]
+  [Alias('T')]
+  [string[]]$To,
+
+  [ValidatePattern('^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{1,}$')]
+  [Alias('C', 'Copy')]
+  [string[]]$Cc,
+
+  [ValidatePattern('^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{1,}$')]
+  [Alias('BC', 'HideCopy')]
+  [string[]]$Bcc,
+
+  [ValidateSet('Low', 'Normal', 'High')]
+  [Alias('P')]
+  [string]$Priority = 'Normal',
+
+  [switch]$SSL,
+  [switch]$BypassCertValid
 )
 
 $CFG = ((Get-Item "${PSCommandPath}").Basename + '.ini')
 $P = (Get-Content -Path "${PSScriptRoot}\${CFG}" | ConvertFrom-StringData)
+$LOG = "${PSScriptRoot}\log.mail.txt"
 $UUID = (Get-CimInstance 'Win32_ComputerSystemProduct' | Select-Object -ExpandProperty 'UUID')
 $HID = ((${Hostname} + ':' + ${UUID}).ToUpper())
+$DATE = (Get-Date -Format 'yyyy-MM-ddTHH:mm:ssK')
+$NL = [Environment]::NewLine
 
 # -------------------------------------------------------------------------------------------------------------------- #
 # -----------------------------------------------------< SCRIPT >----------------------------------------------------- #
 # -------------------------------------------------------------------------------------------------------------------- #
 
-function Start-Smtp {
-  param(
-    [Alias('S')][string]$Subject,
-    [Alias('B')][string]$Body
-  )
-
-  $SmtpClient = New-Object Net.Mail.SmtpClient("$($P.Server)", "$($P.Port)")
-  $SmtpClient.EnableSsl = $SSL
-  $SmtpClient.Credentials = New-Object System.Net.NetworkCredential("$($P.User)", "$($P.Password)")
-  $SmtpClient.Send("$($P.From)", "$($P.To)", "${Subject}", "${Body}")
-}
-
-function Send-BackupError() {
-  $Subject = "Windows Server Backup: ${Hostname}"
+function Write-BackupError() {
   $Body = @"
 Windows Server Backup failed: ${Hostname}.
 Please check server backup!
@@ -69,32 +89,35 @@ Please check server backup!
 Host: ${Hostname}
 Status: ERROR
 
--- 
+--
 #ID:${HID}
 #TYPE:BACKUP:ERROR
 "@
-
-  Start-Smtp -S "${Subject}" -B "${Body}"
 }
 
-function Send-BackupSuccess() {
-  $Subject = "Windows Server Backup: ${Hostname}"
+function Write-BackupSuccess() {
   $Body = @"
 Windows Server Backup completed successfully!
 
 Host: ${Hostname}
 Status: SUCCESS
 
--- 
+--
 #ID:${HID}
 #TYPE:BACKUP:SUCCESS
 "@
+}
 
-  Start-Smtp -S "${Subject}" -B "${Body}"
+function Start-Smtp {
+  if ($BypassCertValid) { [System.Net.ServicePointManager]::ServerCertificateValidationCallback = { $true } }
+  $SmtpClient = (New-Object Net.Mail.SmtpClient($P.Server, $P.Port))
+  $SmtpClient.EnableSsl = $SSL
+  $SmtpClient.Credentials = (New-Object System.Net.NetworkCredential($P.User, $P.Password))
+  $SmtpClient.Send()
 }
 
 function Start-Script() {
-  switch ("${Type}") {
+  switch ($Type) {
     'error'   { Send-BackupError }
     'success' { Send-BackupSuccess }
     default   { exit }
